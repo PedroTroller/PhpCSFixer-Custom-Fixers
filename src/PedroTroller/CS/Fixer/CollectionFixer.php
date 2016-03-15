d32b18d7adcb5d7f8b26c20f4d2ac0488e50a652
Pierre PLAZANET <plazanet.pierre@gmail.com>
2016-03-15 10:30:04 +0100

    Fix collection of fixers behavior

--- src/PedroTroller/CS/Fixer/CollectionFixer.php
+++ src/PedroTroller/CS/Fixer/CollectionFixer.php
@@ -66,7 +66,7 @@ class CollectionFixer extends AbstractFixer
             return parent::getPriority();
         }
 
-        return max($priorities);
+        return min($priorities);
     }
 
     /**
