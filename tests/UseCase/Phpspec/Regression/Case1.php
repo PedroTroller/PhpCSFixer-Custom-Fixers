836f8fdd5d173b94bd0bcc83da0e9188ba78558f
Pierre PLAZANET <plazanet.pierre@gmail.com>
2018-08-29 16:41:43 +0200

    Fix properties reversing with phpspec fixer (#46)

---
+++ tests/UseCase/Phpspec/Regression/Case1.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tests\UseCase\Phpspec\Regression;
+
+use tests\UseCase;
+use PedroTroller\CS\Fixer\PhpspecFixer;
+
+final class Case1 implements UseCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFixer()
+    {
+        $fixer = new PhpspecFixer();
+
+        $fixer->configure([
+            'instanceof' => ['Funk\Spec'],
+        ]);
+
+        return $fixer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRawScript()
+    {
+        return file_get_contents(sprintf('%s/Case1/file.php.txt', __DIR__));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExpectation()
+    {
+        return file_get_contents(sprintf('%s/Case1/file.php.txt', __DIR__));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMinSupportedPhpVersion()
+    {
+        return 0;
+    }
+}
